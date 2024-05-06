06df7f00448f9d3412426097a13b69341432a63e
Denomycor <afonsoresteves@hotmail.com>
2024-05-06 15:37:29 +0100

    Updated DAP configs

--- .config/nvim/lua/kickstart/plugins/debug.lua
+++ .config/nvim/lua/kickstart/plugins/debug.lua
@@ -26,8 +26,6 @@ return {
     'leoluz/nvim-dap-go',
   },
 
-  lazy = true,
-
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
@@ -54,10 +52,14 @@ return {
     vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>B', function()
+    vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+    vim.keymap.set('n', '<leader>dB', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
     end, { desc = 'Debug: Set Breakpoint' })
+    vim.keymap.set('n', '<F6>', function()
+      dap.terminate()
+      dapui.close()
+    end, { desc = 'Debug: Terminate' })
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
