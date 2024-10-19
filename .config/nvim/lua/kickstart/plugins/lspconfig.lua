5d5017a3f730e893892a3947d1c62ccca9b560f4
Denomycor <afonsoresteves@hotmail.com>
2024-10-19 16:39:52 +0100

    Updated config

--- .config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -32,7 +32,7 @@ return {
           map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
           map('gT', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
           map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          map('<leader>ps', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[P]roject [S]ymbols')
           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
           map('K', vim.lsp.buf.hover, 'Hover Documentation')
